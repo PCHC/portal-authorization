35986aa0833d9aa9c8f2962ec7cca65a178ecf4f
Chris Violette <cvioletterun@gmail.com>
2015-09-29 12:13:44 -0400

    Updated authorization form

--- resources/views/patients/authorize.blade.php
+++ resources/views/patients/authorize.blade.php
@@ -5,7 +5,7 @@
 @section('content')
   <div class="row">
     <div class="col-sm-8 col-sm-offset-2">
-      <h1>Authorization Form</h1>
+      <h1>AUTHORIZATION for Secure Website Communication – PCHC</h1>
       {!! Form::open(array(
         'action' => array(
           'PatientController@postAuthorize', $token
@@ -13,23 +13,48 @@
         'class' => 'form-horizontal'
       )) !!}
       {!! Form::hidden('id', $patient->id) !!}
-      <p class="lead">{{ $patient->first_name }} {{ $patient->last_name }}<br />
+      <p class="lead">Patient Name: {{ $patient->first_name }} {{ $patient->last_name }}<br />
       <small>Date of Birth: {{ Carbon::createFromFormat('Y-m-d', $patient->dob)->toFormattedDateString() }}</small></p>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <p>By signing this authorization form, I give permission for my provider to manage my medical information for the following purposes:</p>
 
-      <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+      <ul>
+        <li>Allow me to access to my medical records via eHealth, a secure website</li>
+      </ul>
 
+      <p>I understand that my Penobscot Community Health Care (PCHC) provider uses computers and other electronic systems to manage and store my medical records and that these systems help my provider give me high-quality, cost-effective care.</p>
+
+      <ul>
+        <li>I understand that the use of my provider’s electronic system is not intended for urgent messages or to provide emergency support in life threatening or critical situations. </li>
+        <li>I understand that either my provider or I may choose to stop the use of email communication at any time by contacting the other in writing, by fax or by email. </li>
+        <li>I understand I am responsible for changing my user name and/or password at any time I feel it is no longer confidential. </li>
+        <li>I understand that I am must tell my provider if my e-mail address changes to ensure the correct e-mail address is listed in my medical record.</li>
+      </ul>
+
+      <p>My provider has assured me that all electronic transmissions of medical information follow the required security standards in order to maintain the confidentiality and integrity of my medical information and prevent unauthorized access. I understand I am responsible for the security of my user name and my password. These are needed to access my medical record.</p>
+
+      <p>I authorize my provider to send my medical records to the email address I provide below and any email address I provide in the future, and not to any unauthorized third parties.</p>
+
+      <p>Before signing this Authorization Form, I was made aware of PCHC’s Notice of Privacy Practices. I have been given the opportunity to read the Notice of Privacy Practices or had it explained to me.</p>
+
+      <p>This Authorization is active until canceled in writing on paper or online. <strong>Parent and legal guardian access to a minor patient’s portal is automatically restricted after age 11, unless permission from both the patient and the legal guardian is received.</strong> By signing this form, I do not in any way waive my physician-patient privileges.</p>
+      <ul>
+        <li>I understand that I may obtain a copy of this authorization at any time.</li>
+      </ul>
+
+      <div class="well">
       <div class="form-group">
-        <div class="col-xs-11 col-xs-offset-1">
-          <label>
-            {!! Form::checkbox('auth_confirmed', TRUE) !!} I authorize this thing.
+        <div class="col-xs-12">
+          <label class="checkbox-inline">
+             {!! Form::checkbox('auth_confirmed', TRUE) !!} <strong>I have read this form and had all of my questions answered about using email messaging to establish communication with PCHC providers.<br><br>
+             I understand the terms outlined in this form, and I consent to the use of email and other methods of communication with PCHC providers.</strong>
           </label>
          </div>
        </div>
+      </div>
 
       <div class="form-group">
-        <div class="col-xs-11 col-xs-offset-1">
+        <div class="col-xs-12">
           {!! Form::button('Submit', array(
             'type'=>'submit',
             'class'=>'btn btn-primary'
@@ -37,6 +62,8 @@
         </div>
       </div>
       {!! Form::close() !!}
+
+      @include('patients.instructions')
     </div>
   </div>
 @endsection
---
+++ resources/views/patients/instructions.blade.php
@@ -0,0 +1,108 @@
+<hr>
+
+<p>
+    <a class="btn btn-primary btn-block" role="button" data-toggle="collapse" href="#portalInstructions" aria-expanded="false" aria-controls="portalInstructions">
+      <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span> Instructions for Communicating Using the PCHC Secure Website
+  </a>
+</p>
+<div class="collapse" id="portalInstructions">
+  <div class="panel panel-primary">
+    <div class="panel-heading">
+      <h4>Instructions for Communicating Using the PCHC Secure Website</h4>
+    </div>
+    <div class="panel-body">
+      <p>For your convenience and to enhance your care experience, PCHC offers you the option of using its patient portal, known as “eHealth.”  eHealth allows you to communicate with us, to schedule appointments and view some of your health information online. Below are some basic instructions about using eHealth. The first page contains an authorization to communicate using the secure portal, which you must read and sign while in the PCHC offices before we will activate your access. Please review this document carefully, and feel free to ask us any questions you have about eHealth</p>
+
+      <h4>What is the eHealth? </h4>
+      <p>PCHC's eHealth is a secure website where you may view information from your electronic medical record. With this service, you can:</p>
+      <ul>
+        <li>Update your personal information, such as address and telephone number</li>
+        <li>Request an appointment online</li>
+        <li>Renew a prescription online</li>
+        <li>Send a non-urgent message to your provider</li>
+        <li>View your electronic medical record</li>
+        <li>Request referral information</li>
+        <li>Send messages to the billing office</li>
+      </ul>
+
+      <p>If you would like to use this service, please read and sign the attached authorization form to show that you understand and agree to the rules below. The form must be signed in the office and in front of a staff member. </p>
+
+      <h4>Will my information be secure?</h4>
+      <p>We have taken steps to ensure that information sent through this online service is secure. To make your health information more secure, you must sign the attached authorization form and get your password from the office in person. Do not share this password with anyone, as it will allow access to some of your private health information.</p>
+
+      <p>We cannot guarantee the confidentiality of your email once it arrives in your in-box. We only send email through a secure server, which means the email is encrypted and, therefore, reasonably secure. However, there are some special issues with email you need to be aware of:</p>
+      <ul>
+        <li>Your employer typically owns the equipment, servers and all the information contained on them, and it reserves the right to access your email at any time. Once the email arrives in your in-box, therefore, its contents may be viewed by your employer (as part of an audit, for example, or for some other business reason). Accordingly, it is better to use a personal email address in connection with eHealth.</li>
+        <li>If your personal email address is a shared family email address, other family members may see your messages which will contain health information that you may not want disclosed even to family members. To avoid that, ensure your personal email is accessed only by you. </li>
+        <li>You should never share your confidential user name and password with anyone; if you do, that person may see your private health information and communicate with your provider as if they were you. </li>
+        <li>There is a small risk that email messages may be intercepted by others (“hackers”), although if you only send emails through the patient portal your email should remain as safe as it can be from hackers.</li>
+      </ul>
+
+      <p>Also note that PCHC is not responsible for technical difficulties with email communications. PCHC cannot advise you in the use of your own computer or email.</p>
+
+      <h4>How do I communicate with my doctor via email?</h4>
+      <p>All PCHC team members have their own email addresses for your convenience. Many email addresses do not clearly identify the sender. Please make sure your full name is present in the body of the message. Do <strong>NOT</strong> put your name in the subject line. Also, if you have a common name we ask that you also include your date of birth so we can identify you. </p>
+
+      <p>When sending a message to your provider, other PCHC staff members may screen the information first before sending it to the correct person. A copy of your email will be placed unedited in your medical record. </p>
+
+      <p><strong><em>Please keep in mind that although email can be a very effective tool, it is not a substitute for a physical exam by your provider. Please call the office if you feel you need to see your provider.</em></strong></p>
+
+      <p><strong>The following subjects are NEVER okay for email:</strong></p>
+      <ul>
+        <li><strong>Any urgent medical problem or emergency, mental health issue, drug and alcohol problem, HIV/AIDS related issues and diseases, work-related injuries and disability.</strong></li>
+      </ul>
+
+      <h4>Avoiding Spam Problems</h4>
+      <dl class="dl-horizontal">
+        <dt>Problem:</dt>
+        <dd>When we send you a secure message, you should first receive a notification email in the inbox of your email program. In some cases, however, your email program's spam filter sends the notification email to your Spam, Bulk, or Junk email folder.</dd>
+        <dt>Resolution:</dt>
+        <dd>Please see the following table to help resolve issues with your spam filter. If you do not see your program listed, refer to your email program’s 'spam' or 'safe filter' instructions.</dd>
+      </dl>
+
+      <div class="table-responsive">
+        <table class="table table-striped table-hover">
+          <thead>
+          <tr>
+            <th>Email Program</th>
+            <th>Where to Find Misdirected Notification Emails</th>
+            <th>How to Ensure That You Receive Notification Emails in Your Inbox</th>
+          </tr>
+          </thead>
+          <tbody>
+          <tr>
+            <td>AOL®</td>
+            <td>SPAM folder</td>
+            <td>Add the sender of the notification email to your AOL address book.</td>
+          </tr>
+          <tr>
+            <td>Google® Gmail®</td>
+            <td>SPAM folder</td>
+            <td>Add the sender of the notification email to your Gmail contacts list.</td>
+          </tr>
+          <tr>
+            <td>Microsoft® Hotmail®</td>
+            <td>Junk E-Mail folder</td>
+            <td>In Hotmail help, search for “create a safe list;” then follow the instructions in the corresponding topic.</td>
+          </tr>
+          <tr>
+            <td>Microsoft Outlook</td>
+            <td>Junk E-Mail folder</td>
+            <td>In Outlook help, search for “add name to safe senders list;” then follow the instructions in the corresponding topic.</td>
+          </tr>
+          <tr>
+            <td>Microsoft Windows Live Hotmail</td>
+            <td>Junk folder</td>
+            <td>In Windows Live Hotmail help, search for “block or allow;” then follow the instructions in the corresponding topic.</td>
+          </tr>
+          <tr>
+            <td>Yahoo!® Mail</td>
+            <td>Spam or Bulk Mail folder</td>
+            <td>Add the sender of the notification email to your Yahoo! Address book.</td>
+          </tr>
+          </tbody>
+        </table>
+      </div><!-- /.table-responsive -->
+    </div><!-- /.panel-body -->
+  </div><!-- /.panel -->
+</div><!-- /.collapse -->
